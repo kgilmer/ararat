0d3bf8eddb3b014d4bc6fa8b3ffc5c23b3cb4757
Ken Gilmer <kgilmer@gmail.com>
2025-05-11 17:28:11 -0700

    update to layershell window

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,39 @@
-
-use iced::widget::{button, column, text, Column};
-use iced::{application, Center, Font, Pixels, Settings};
+//! Elbey - a desktop app launcher
+use iced::widget::{Column, button, column, text};
+use iced::{Font, Pixels, Task, Theme};
+use iced_layershell::{to_layer_message, Application};
+use iced_layershell::reexport::{Anchor, KeyboardInteractivity, Layer};
+use iced_layershell::settings::{LayerShellSettings, Settings, StartMode};
 use lazy_static::lazy_static;
+use std::sync::{Arc, Mutex};
 
 lazy_static! {
-    static ref PROGRAM_NAME: String = String::from("Ararat");
+    static ref PROGRAM_NAME: String = String::from("Elbey");
+}
+
+/// Program entrypoint.  Just configures the app, window, and kicks off the iced runtime.
+fn main() -> Result<(), iced_layershell::Error> {
+    let iced_settings = Settings {
+        layer_settings: LayerShellSettings {
+            size: Some((320, 200)),
+            exclusive_zone: 200,
+            anchor: Anchor::all(),
+            start_mode: StartMode::Active,
+            layer: Layer::Overlay,
+            margin: (0, 0, 0, 0),
+            keyboard_interactivity: KeyboardInteractivity::Exclusive,
+            events_transparent: false,
+        },
+        flags: Flags {},
+        id: Some(PROGRAM_NAME.to_string()),
+        fonts: vec![],
+        default_font: Font::DEFAULT,
+        default_text_size: Pixels::from(18),
+        antialiasing: true,
+        virtual_keyboard_support: None,
+    };
+
+    Ararat::run(iced_settings)
 }
 
 pub struct Ararat {
@@ -18,58 +47,50 @@ struct Counter {
 }
 
 #[derive(Debug, Clone)]
-pub struct Flags {
+pub struct Flags {}
 
-}
-
-pub fn main() -> iced::Result {
-    // iced::run("A cool counter", Counter::update, Counter::view)
-    
-    let settings: Settings = Settings {
-        id: Some(PROGRAM_NAME.to_string()),
-        fonts: vec![],
-        default_font: Font::MONOSPACE,
-        default_text_size: Pixels::from(18),
-        antialiasing: true,
-    };
-
-
-    let app = 
-    application(PROGRAM_NAME.as_str(), Counter::update, Counter::view).settings(settings);
-
-    app.run()
-    // Ararat::run(settings)
-}
-
-
-
-#[derive(Debug, Clone, Copy)]
-enum Message {
+#[to_layer_message]
+#[derive(Debug, Clone)]
+pub enum Message {
     Increment,
     Decrement,
 }
 
+impl Application for Ararat {
+    type Message = Message;
+    type Flags = Flags;
+    type Theme = Theme;
+    type Executor = iced::executor::Default;
 
-
-impl Counter {
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::Increment => {
-                self.value += 1;
-            }
-            Message::Decrement => {
-                self.value -= 1;
-            }
-        }
+    fn new(flags: Self::Flags) -> (Self, iced::Task<Self::Message>) {
+        (Self { state: Counter::default(), flags }, Task::none() )
     }
 
-    fn view(&self) -> Column<Message> {
+    fn namespace(&self) -> String {
+        PROGRAM_NAME.to_string()
+    }
+
+    fn update(&mut self, message: Self::Message) -> iced::Task<Self::Message> {
+        match message {
+            Message::Increment => {
+                self.state.value += 1;
+            }
+            Message::Decrement => {
+                self.state.value -= 1;
+            }
+            _ => {}
+        }
+        Task::none()
+    }
+
+    fn view(&self) -> iced::Element<'_, Self::Message, Self::Theme, iced::Renderer> {
         column![
             button("Increment").on_press(Message::Increment),
-            text(self.value).size(50).font(Font::MONOSPACE),
+            text(self.state.value).size(50).font(Font::MONOSPACE),
             button("Decrement").on_press(Message::Decrement)
         ]
         .padding(20)
-        .align_x(Center)
+        .align_x(iced::Alignment::Center)
+        .into()
     }
 }
